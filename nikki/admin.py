99cf45987877b0345ae2e44f62d97b525fafeeef
Atheenaxs <athenashl23@gmail.com>
2025-05-08 23:33:59 +0200

    add models to admin interface

--- nikki/admin.py
+++ nikki/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Anime, UserAnime, Season, Episode, UserEpisodeView, UserSeasonView
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Anime)
+admin.site.register(UserAnime)
+admin.site.register(Season)
+admin.site.register(Episode)
+admin.site.register(UserEpisodeView)
+admin.site.register(UserSeasonView)
