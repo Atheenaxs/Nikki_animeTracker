8a122924286a92145f60aba79c1f36a462c37cd3
Atheenaxs <athenashl23@gmail.com>
2025-05-08 23:45:40 +0200

    fix admin file

--- nikki/admin.py
+++ nikki/admin.py
@@ -2,14 +2,6 @@ from django.contrib import admin
 from .models import User, Anime, UserAnime, Season, Episode, UserEpisodeView, UserSeasonView
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Anime)
-admin.site.register(UserAnime)
-admin.site.register(Season)
-admin.site.register(Episode)
-admin.site.register(UserEpisodeView)
-admin.site.register(UserSeasonView)
-
 #custom
 class AnimeAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'release_date', 'image_preview')  # Affiche l'image et d'autres champs
