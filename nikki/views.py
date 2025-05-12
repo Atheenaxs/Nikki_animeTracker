cd1ae7d98c70ab74f21699400bcafc629f757275
Atheenaxs <athenashl23@gmail.com>
2025-05-12 14:44:10 +0200

    Implement home view using Jikan API

--- nikki/views.py
+++ nikki/views.py
@@ -1,3 +1,12 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    response = requests.get("https://api.jikan.moe/v4/top/anime")
+    top_animes = []
+
+    if response.status_code == 200:
+        data = response.json()
+        top_animes = data.get("data", [])[:10]  # on prend les 10 premiers
+
+    return render(request, "animes/home.html", {"top_animes": top_animes})
