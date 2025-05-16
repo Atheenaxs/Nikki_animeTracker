263d60029d9c70dfc383b61264202866518f80d6
Atheenaxs <athenashl23@gmail.com>
2025-05-16 16:29:03 +0200

    link front and back signup

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from nikki import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
     path("conditions-utilisation/", views.terms, name="terms"),
     path("politique-confidentialite/", views.privacy, name="privacy"),
     path("genres/<slug:slug>/", views.genre_animes, name="genre_animes"),
